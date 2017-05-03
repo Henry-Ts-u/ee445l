09375b0383da3e37634a25f924baccf099c2e91b
Henry-Ts-u <tsougreen@gmail.com>
2017-05-03 14:03:21 -0500

    lives
    added 3 lives, game stops when character hit 3x

--- lab_11_graphics_test_ST7735_4C123/ST7735TestMain.c
+++ lab_11_graphics_test_ST7735_4C123/ST7735TestMain.c
@@ -859,35 +859,38 @@ const uint16_t black[] = {
 int main(void){//note LCD is 160x128
 	PLL_Init(Bus80MHz); 
 	ST7735_InitR(INITR_REDTAB);
-  //ST7735_DrawBitmap(59, 120, character, 20, 20);   // top left corner of the screen
+  ST7735_DrawBitmap(64, 150, character, 20, 20);   // top left corner of the screen
   //ST7735_DrawBitmap(59, 74, character, 80, 80); // center of the screen
-	int x, y, dy, x2, y2, x3, y3, lvl, score, count;
-	x = 15; y = 25; dy = 1; x2 = 100; y2 = 80; x3 = 50; y3 = 120; lvl = 1; count = score = 0;
-	printf("lvl 1");
-	//printf("\ncurrent score %d", score);
+	int x, y, dy, x2, y2, x3, y3, lvl, score, count, charx, lives;
+	x = 15; y = 25; dy = 1; x2 = 100; y2 = 80; x3 = 50; y3 = 120; lvl = 1; count = score = 0; lives = 3;
+	charx = 64; 
+	printf("lvl 1 score 0");
 	while(1)
 	{
 		y = y + dy;
 		y2 = y2 + dy;
 		y3 = y3 + dy;
-		if(y > 140){
+		if(y > 150){
 			y = 0;
 			x = rand()%108;
-			ST7735_DrawBitmap(0, 140, black, 128, 20);
+			ST7735_DrawBitmap(0, 150, black, 128, 20);
+			ST7735_DrawBitmap(charx, 150, character, 20, 20); 
 			count = count + 1;
 			score = score + 1;
 		}
-		if(y2 > 140){
+		if(y2 > 150){
 			y2 = 0;
 			x2 = rand()%108;
-			ST7735_DrawBitmap(0, 140, black, 128, 20);
+			ST7735_DrawBitmap(0, 150, black, 128, 20);
+			ST7735_DrawBitmap(charx, 150, character, 20, 20); 
 			count = count + 1;
 			score = score + 1;
 		}
-		if(y3 > 140){
+		if(y3 > 150){
 			y3 = 0;
 			x3 = rand()%108;
-			ST7735_DrawBitmap(0, 140, black, 128, 20);
+			ST7735_DrawBitmap(0, 150, black, 128, 20);
+			ST7735_DrawBitmap(charx, 150, character, 20, 20); 
 			count = count + 1;
 			score = score + 1;
 		}
@@ -900,113 +903,40 @@ int main(void){//note LCD is 160x128
 				printf("lvl %d", lvl);
 			}
 		}
+		//check if player got hit
+		if(y==130){
+			if(abs(x-charx)<15){
+				lives = lives - 1;
+				if(lives == 0){
+					break;
+				}
+			}
+		}
+		if(y2==130){
+			if(abs(x2-charx)<15){
+				lives = lives - 1;
+				if(lives == 0){
+					break;
+				}
+			}
+		}
+		if(y3==130){
+			if(abs(x3-charx)<15){
+				lives = lives - 1;
+				if(lives == 0){
+					break;
+				}
+			}
+		}
 		 DelayWait10ms(20-2*lvl);
 		 ST7735_DrawBitmap(x, y, green, 20, 20);
 		 ST7735_DrawBitmap(x2, y2, blue, 20, 20);
 		 ST7735_DrawBitmap(x3, y3, red, 20, 20);
 		 ST7735_SetCursor(0, 0);
-		 printf("lvl %d\n", lvl);
+		 printf("lvl %d score %d", lvl, score);
 	}
 }
-int mainx(void){  // main 2
-  int x, y, dx, dy;
-//  uint8_t red, green, blue;
-  PLL_Init(Bus80MHz);                  // set system clock to 80 MHz
-  // test DrawChar() and DrawCharS()
-  ST7735_InitR(INITR_REDTAB);
-  ST7735_DrawCharS(0, 0, 'c', ST7735_Color565(255, 0, 0), 0, 1);
-  ST7735_DrawCharS(6, 0, 'h', ST7735_Color565(255, 128, 0), 0, 1);
-  ST7735_DrawCharS(12, 0, 'a', ST7735_Color565(255, 255, 0), 0, 1);
-  ST7735_DrawCharS(18, 0, 'r', ST7735_Color565(128, 255, 0), 0, 1);
-  ST7735_DrawCharS(30, 0, 'T', ST7735_Color565(0, 255, 0), 0, 1);
-  ST7735_DrawCharS(36, 0, 'E', ST7735_Color565(0, 255, 255), 0, 1);
-  ST7735_DrawCharS(42, 0, 'S', ST7735_Color565(0, 0, 255), 0, 1);
-  ST7735_DrawCharS(48, 0, 'T', ST7735_Color565(128, 0, 255), 0, 1);
 
-  ST7735_DrawCharS(0, 8, 'c', ST7735_Color565(255, 0, 0), 0, 2);
-  ST7735_DrawCharS(12, 8, 'h', ST7735_Color565(255, 128, 0), 0, 2);
-  ST7735_DrawCharS(24, 8, 'a', ST7735_Color565(255, 255, 0), 0, 2);
-  ST7735_DrawCharS(36, 8, 'r', ST7735_Color565(128, 255, 0), 0, 2);
-  ST7735_DrawCharS(60, 8, 'T', ST7735_Color565(0, 255, 0), 0, 2);
-  ST7735_DrawCharS(72, 8, 'E', ST7735_Color565(0, 255, 255), 0, 2);
-  ST7735_DrawCharS(84, 8, 'S', ST7735_Color565(0, 0, 255), 0, 2);
-  ST7735_DrawCharS(96, 8, 'T', ST7735_Color565(128, 0, 255), 0, 2);
-
-  ST7735_DrawCharS(0, 24, 'c', ST7735_Color565(255, 0, 0), 0, 3);
-  ST7735_DrawCharS(18, 24, 'h', ST7735_Color565(255, 128, 0), 0, 3);
-  ST7735_DrawCharS(36, 24, 'a', ST7735_Color565(255, 255, 0), 0, 3);
-  ST7735_DrawCharS(54, 24, 'r', ST7735_Color565(128, 255, 0), 0, 3);
-  ST7735_DrawCharS(90, 24, 'T', ST7735_Color565(0, 255, 0), 0, 3);
-  ST7735_DrawCharS(108, 24, 'E', ST7735_Color565(0, 255, 255), 0, 3);
-  ST7735_DrawCharS(126, 24, 'S', ST7735_Color565(0, 0, 255), 0, 3); // off the screen
-  ST7735_DrawCharS(144, 24, 'T', ST7735_Color565(128, 0, 255), 0, 3); // totally off the screen
-
-  ST7735_DrawCharS(0, 48, 'c', ST7735_Color565(255, 0, 0), 0, 4);
-  ST7735_DrawCharS(24, 48, 'h', ST7735_Color565(255, 128, 0), 0, 4);
-  ST7735_DrawCharS(48, 48, 'a', ST7735_Color565(255, 255, 0), 0, 4);
-  ST7735_DrawCharS(72, 48, 'r', ST7735_Color565(128, 255, 0), 0, 4);
-  ST7735_DrawCharS(120, 48, 'T', ST7735_Color565(0, 255, 0), 0, 4); // off the screen
-  ST7735_DrawCharS(144, 48, 'E', ST7735_Color565(0, 255, 255), 0, 4); // totally off the screen
-  ST7735_DrawCharS(168, 48, 'S', ST7735_Color565(0, 0, 255), 0, 4); // totally off the screen
-  ST7735_DrawCharS(192, 48, 'T', ST7735_Color565(128, 0, 255), 0, 4); // totally off the screen
-
-  ST7735_DrawCharS(0, 80, 'c', ST7735_Color565(255, 0, 0), 0, 6);
-  ST7735_DrawCharS(36, 80, 'h', ST7735_Color565(255, 128, 0), 0, 6);
-  ST7735_DrawCharS(72, 80, 'a', ST7735_Color565(255, 255, 0), 0, 6);
-  ST7735_DrawCharS(108, 80, 'r', ST7735_Color565(128, 255, 0), 0, 6); // off the screen
-
-  ST7735_DrawChar(0, 135, 'A', ST7735_Color565(255, 128, 128), 0, 2);
-  ST7735_DrawChar(12, 135, 'd', ST7735_Color565(255, 0, 0), 0, 2);
-  ST7735_DrawChar(24, 135, 'v', ST7735_Color565(255, 128, 0), 0, 2);
-  ST7735_DrawChar(48, 135, 'c', ST7735_Color565(255, 255, 0), 0, 2);
-  ST7735_DrawChar(60, 135, 'h', ST7735_Color565(128, 255, 0), 0, 2);
-  ST7735_DrawChar(72, 135, 'a', ST7735_Color565(0, 255, 0), 0, 2);
-  ST7735_DrawChar(84, 135, 'r', ST7735_Color565(0, 255, 255), 0, 2);
-  ST7735_DrawChar(108, 135, 't', ST7735_Color565(0, 0, 255), 0, 2);
-  ST7735_DrawChar(120, 135, 'e', ST7735_Color565(128, 0, 255), 0, 2); // off the screen
-  ST7735_DrawChar(132, 135, 's', ST7735_Color565(255, 0, 255), 0, 2); // totally off the screen
-  ST7735_DrawChar(144, 135, 't', ST7735_Color565(255, 255, 255), 0, 2); // totally off the screen
-
-  ST7735_DrawChar(0, 151, 'A', ST7735_Color565(255, 128, 128), 0, 1);
-  ST7735_DrawChar(6, 151, 'd', ST7735_Color565(255, 0, 0), 0, 1);
-  ST7735_DrawChar(12, 151, 'v', ST7735_Color565(255, 128, 0), 0, 1);
-  ST7735_DrawChar(24, 151, 'c', ST7735_Color565(255, 255, 0), 0, 1);
-  ST7735_DrawChar(30, 151, 'h', ST7735_Color565(128, 255, 0), 0, 1);
-  ST7735_DrawChar(36, 151, 'a', ST7735_Color565(0, 255, 0), 0, 1);
-  ST7735_DrawChar(42, 151, 'r', ST7735_Color565(0, 255, 255), 0, 1);
-  ST7735_DrawChar(54, 151, 't', ST7735_Color565(0, 0, 255), 0, 1);
-  ST7735_DrawChar(60, 151, 'e', ST7735_Color565(128, 0, 255), 0, 1);
-  ST7735_DrawChar(66, 151, 's', ST7735_Color565(255, 0, 255), 0, 1);
-  ST7735_DrawChar(72, 151, 't', ST7735_Color565(255, 255, 255), 0, 1);
-  DelayWait10ms(100);
-
-  // test DrawBitmap() including clipping test with images off the screen
-  ST7735_FillScreen(0xFFFF);            // set screen to white
-  ST7735_DrawBitmap(0, 5, Test2, 5, 6);   // top left corner of the screen
-  ST7735_DrawBitmap(59, 74, Test2, 5, 6); // center of the screen
-  ST7735_DrawBitmap(125, 74, Test2, 5, 6);// clip right
-  ST7735_DrawBitmap(59, 162, Test2, 5, 6);// clip bottom
-  ST7735_DrawBitmap(-2, 74, Test2, 5, 6); // clip left
-  ST7735_DrawBitmap(59, 2, Test2, 5, 6);  // clip top
-  DelayWait10ms(100);
-
-  ST7735_FillScreen(0xFFFF);            // set screen to white
-  x = 44;
-  y = 159;
-  dx = 1;
-  dy = 1;
-  while(1){
-    ST7735_DrawBitmap(x, y, Logo, 40, 160);
-    x = x + dx;
-    y = y + dy;
-    if((x >= (ST7735_TFTWIDTH - 40 + 15)) || (x <= -15)){
-      dx = -1*dx;
-    }
-    if((y >= (ST7735_TFTHEIGHT + 8)) || (y <= (ST7735_TFTHEIGHT - 8))){
-      dy = -1*dy;
-    }
-  }
-}
 // private function draws a color band on the screen
 void static drawthecolors(uint8_t red, uint8_t green, uint8_t blue){
   static uint16_t y = 0;
@@ -1017,61 +947,7 @@ void static drawthecolors(uint8_t red, uint8_t green, uint8_t blue){
   }
   DelayWait10ms(1);
 }
-int main3(void){ // main3
-  uint8_t red, green, blue;
-  PLL_Init(Bus80MHz);                  // set system clock to 80 MHz
-  // test DrawChar() and DrawCharS()
-  ST7735_InitR(INITR_REDTAB);
 
-  // test display with a colorful demo
-  red = 255;
-  green = 0;
-  blue = 0;
-  while(1){
-    // transition from red to yellow by increasing green
-    for(green=0; green<255; green=green+1){
-      drawthecolors(red, green, blue);
-    }
-    // transition from yellow to green by decreasing red
-    for(red=255; red>0; red=red-1){
-      drawthecolors(red, green, blue);
-    }
-    // transition from green to light blue by increasing blue
-    for(blue=0; blue<255; blue=blue+1){
-      drawthecolors(red, green, blue);
-    }
-    // transition from light blue to true blue by decreasing green
-    for(green=255; green>0; green=green-1){
-      drawthecolors(red, green, blue);
-    }
-    // transition from true blue to pink by increasing red
-    for(red=0; red<255; red=red+1){
-      drawthecolors(red, green, blue);
-    }
-    // transition from pink to red by decreasing blue
-    for(blue=255; blue>0; blue=blue-1){
-      drawthecolors(red, green, blue);
-    }
-  }
-}
-void BookExamples(void){ // examples from the book
-  int8_t cc = 0x56; // (‘V’)
-  int32_t xx = 100;
-  int16_t yy = -100;
-  float zz = 3.14159265;
-  printf("Hello world\n");      //Hello world
-  printf("cc = %c %d\n",cc,cc);  //cc = V 86
-  printf("cc = %#x\n",cc);      //cc = 0x56
-  printf("xx = %c %d\n",xx,xx);  //xx = d 100
-  printf("xx = %#x\n",xx);      //xx = 0x64
-  printf("yy = %d\n",yy);        //yy = -100
-  printf("%#x   \n",yy);        //yy = 0xffffff9c
-  printf("%e \n",zz);            //zz = 3.14159e+00
-  printf("%E \n",zz);            //zz = 3.14159E+00
-  printf("%f     \n",zz);        //zz = 3.14159
-  printf("%g     \n",zz);        //zz = 3.14159 (shorter of two, either f or e)
-  printf("%3.2f     \n",zz);    //zz =  3.14
-}
 #define PF2   (*((volatile uint32_t *)0x40025010))
 
 // Make PF2 an output, enable digital I/O, ensure alt. functions off
@@ -1088,38 +964,6 @@ void SSR_Init(void){
 
 
 void Delay1ms(uint32_t n);
-int main4(void){
-  SSR_Init();
-  while(1){
-    Delay1ms(10);
-    PF2 ^= 0x04;
-  }
-}
-int main5(void){
-  SSR_Init();
-  while(1){
-    DelayWait10ms(1000);
-    PF2 ^= 0x04;
-  }
-}
-int main6(void){ int32_t i,n; // main 6
-  Output_Init();              // initialize output device
-  Output_Color(ST7735_YELLOW);
-  BookExamples();
-  n = 0;
-  while(1){
-    printf("\ni=");
-    for(i=0; i<1; i++){
-      printf("%d ",i+n);
-    }
-
-    n = n+10000000; // notice what happens when this goes above 2,147,483,647
-  }
-}
-// Subroutine to wait 10 msec
-// Inputs: None
-// Outputs: None
-// Notes: ...
 void DelayWait10ms(uint32_t n){uint32_t volatile time;
   while(n){
     time = 727240*2/91;  // 10msec
